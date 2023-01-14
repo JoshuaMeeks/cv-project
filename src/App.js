c097645254f65f658e9f7649ac92f27eb73b0bc4
Your Name <jlmeeks587@gmail.com>
2023-01-13 20:34:52 -0500

    Setting up useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Form } from "./components/Form";
@@ -5,6 +6,13 @@ import { CVProfile } from "./components/CVProfile";
 
 
 function App() {
+  const [personal, setPersonal] = useState({
+    firstName: '',
+    lastName: '',
+    Address: '',
+    Number: '',
+    Email: ''
+  })
 
   return (
     <div className="App">
